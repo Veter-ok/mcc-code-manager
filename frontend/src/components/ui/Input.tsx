5130d938539aaf5697741dcf4118b9714791de43
Lavrov Rodion <lrd.msk@gmail.com>
2024-08-16 00:08:59 +0300

    Add custom input component

---
+++ frontend/src/components/ui/Input.tsx
@@ -0,0 +1,17 @@
+interface IInputProps {
+    value: string
+    onChange(v: string): void
+    placeholder?: string
+    style?: string
+}
+
+export const Input:React.FC<IInputProps> = ({value, onChange, placeholder, style}) => {
+    return (
+        <input 
+            className={`w-[400px] h-[40px] p-2 text-lg border-primary border-4 rounded-md ${style}`}
+            placeholder={placeholder}
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+        />
+    )
+}
